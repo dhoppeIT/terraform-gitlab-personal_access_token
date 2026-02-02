e3b62bdbf4d0c6f1fce43ffb198d148c6745815d
Dennis Hoppe <dennis.hoppe@dhoppe.it>
2026-02-02 11:25:35 +0100

    fix(terraform): Add missing variable

--- main.tf
+++ main.tf
@@ -3,8 +3,9 @@ resource "gitlab_personal_access_token" "this" {
   scopes  = var.scopes
   user_id = var.user_id
 
-  expires_at  = var.expires_at
-  description = var.description
+  expires_at                    = var.expires_at
+  description                   = var.description
+  validate_past_expiration_date = var.validate_past_expiration_date
 
   rotation_configuration = {
     expiration_days    = try(var.rotation_configuration["expiration_days"], null)
